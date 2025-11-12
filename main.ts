b9e60d86adc87b2fab4c4b418e99372ab0317add
default <zoe.wrubleski@stu.ocsb.ca>
2025-11-12 14:10:55 +0000

    declaring variables in ts

---
+++ main.ts
@@ -0,0 +1,34 @@
+/**
+ * @author Zoe Wrubleski
+ * @version 1.0.0
+ * @date 2025-11-12
+ * @fileoverview This program shows variable declarations. 
+ */
+
+// number data type count
+let count: number = 20;
+
+// number data type big_number
+let big_number: number = 14500000000;
+
+// number data type temperature
+let temperature: number = -32.4;
+
+// number data type molecules
+let molecules: number = 158799625.35;
+
+// string data type
+let courseCode: string = "ICS3U";
+
+// boolean data type play_again
+let play_again: boolean = true;
+
+// display each data type to the screen
+console.log("Can you count up to " + count + "?");
+console.log("This is a really big number " + big_number);
+console.log("The outside temperature currently is " + temperature);
+console.log("A drop of water might have " + molecules + " molecules");
+console.log("Do you like " + courseCode + "?");
+console.log("True/False Computer programming is cool: " + play_again);
+
+console.log("\nDone");
